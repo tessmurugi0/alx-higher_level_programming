a2f5a2544654d9cc717cb3e42ef28cbbffcdf0ed
Tess <tesstera34@gmail.com>
2023-10-31 01:35:45 -0700

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""define rectangle"""
+
 class Rectangle:
     """this represents a rectangle"""
     def __init__(self, width=0, height=0):
@@ -12,11 +14,26 @@ class Rectangle:
     """
     self._width = width
     self._height = height
-@property
-def width(self):
+
+    @property
+    def width(self):
     """retrives the width atribute"""
     return self._width
-@width.setter
-def width(self, value):
+
+    @width.setter
+    def width(self, value):
     """set attribute"""
-    if not isinstance
+    if not isinstance(value, int):
+        raise TypeError("width must be an integer")
+    if value < 0:
+        raise ValueError("width must be >= 0")
+    self._width = value
+
+    @height.setter
+    def height(self, value):
+        if not isintance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height value
+
