92f849435fc5e0bc9f54452bcc39678d0071d8b9
Tess <tesstera34@gmail.com>
2023-10-31 01:49:10 -0700

    Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
-"""define rectangle"""
+"""Define rectangle"""
 
 class Rectangle:
-    """this represents a rectangle"""
+    """This represents a rectangle"""
     def __init__(self, width=0, height=0):
         """Initializing the rectangle class
-    Args:
-        width: width of the rectangle
-        height: height of the rectangle
-    Raises:
-        TypeError: if size is not an integer
-        ValueError: if size is less than zero
-    """
-    self._width = width
-    self._height = height
+        Args:
+            width: width of the rectangle
+            height: height of the rectangle
+        Raises:
+            TypeError: if size is not an integer
+            ValueError: if size is less than zero
+        """
+        self._width = width
+        self._height = height
 
     @property
     def width(self):
-    """retrives the width atribute"""
-    return self._width
+        """Retrieves the width attribute"""
+        return self._width
 
     @width.setter
     def width(self, value):
-    """set attribute"""
-    if not isinstance(value, int):
-        raise TypeError("width must be an integer")
-    if value < 0:
-        raise ValueError("width must be >= 0")
-    self._width = value
+        """Set attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """Retrieves the height attribute"""
+        return self._height
 
     @height.setter
     def height(self, value):
-        if not isintance(value, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height value
+        self._height = value
 
